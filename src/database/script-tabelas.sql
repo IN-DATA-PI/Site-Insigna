e23ddba85a6f83fb3d943c06ae827cac1a8262fb
Victor Henrique <103295994+Vitinn04@users.noreply.github.com>
2024-10-07 20:02:22 -0300

    tabelas do banco

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -11,12 +11,42 @@ use Insigna;
 
 create table usuario(
 id int primary key auto_increment,
-nome varchar(50),
-departamento varchar(50),
+nome varchar(50) unique,
+distritoPolicial int,
 email varchar(45),
-matricula int,
+matricula varchar(20),
 senha varchar(45)
 );
 
+insert into usuario(nome, matricula, email, senha) values ("Victor", "1234567", "victor@gmail.com", "1234567");
+
+create table operacoes(
+codigoOp int auto_increment primary key,
+nomeOp varchar(45),
+qtdPoliciais int,
+localOp varchar(45),
+dataOp date,
+descricaoOp varchar(500));
+
+create table dados(
+id int primary key auto_increment,
+dp varchar(100),
+natureza varchar(100),
+ano int,
+janeiro int,
+fevereiro int,
+marco int,
+abril int,
+maio int,
+junho int,
+julho int,
+agosto int,
+setembro int,
+outubro int,
+novembro int,
+dezembro int,
+total int );
 
 select * from usuario;
+select * from operacoes;
+select * from dados;
