ba5c26140efaaae60e8f4f75ddf222201120b942
JoaoKeviin <joao.matos@sptech.school>
2024-10-21 22:09:24 -0300

    debug: insert cadastro

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -20,7 +20,7 @@ function cadastrar(nome, distritoPolicial, email, matricula, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, departamento, email, matricula, senha) VALUES ('${nome}', '${distritoPolicial}', '${email}', '${matricula}', '${senha}');
+        INSERT INTO usuario (nome, distritoPolicial, email, matricula, senha) VALUES ('${nome}', '${distritoPolicial}', '${email}', '${matricula}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
